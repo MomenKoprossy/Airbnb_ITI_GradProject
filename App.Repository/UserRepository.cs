c205a403b0c46ba9b56f549a869ac8b310d05497
Omar Henawy <omarhenawy31@gmail.com>
2021-07-22 14:05:17 +0200

    RepositoryPattern

---
+++ App.Repository/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using App.Repository.ApiClient;
+using Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repository
+{
+     public class UserRepository
+    {
+        private readonly IWebApiExecuter webApiExecuter;
+
+         public UserRepository (IWebApiExecuter webApiExecuter) //implementing the dependency injection 
+        {
+            this.webApiExecuter = webApiExecuter;
+        }
+        public async Task<IEnumerable<User>> Get()
+        {
+            return await webApiExecuter.InvokeGet<IEnumerable<User>>("api/users");
+        }
+    }
+}
---
+++ Testing.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using App.Repository;
+using App.Repository.ApiClient;
+using System.Threading.Tasks;
+using System;
+using System.Net.Http;
+
+HttpClient httpClient = new();
+IWebApiExecuter apiExecuter = new WebApiExecuter("https://localhost:44351/", httpClient);
+await GetUsers();
+
+async Task GetUsers()
+{
+    UserRepository repository = new(apiExecuter);
+    var users = await repository.Get();
+    foreach(var user in users)
+    {
+        Console.WriteLine($"Users: {user.Fname}");
+    }
+}
