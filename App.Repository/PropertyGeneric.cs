4378ceb3250f9a9fc83561041b132ff9538ae612
Your Name <you@example.com>
2021-07-26 21:41:27 +0200

    updated with property generic

---
+++ App.Repository/PropertyGeneric.cs
@@ -0,0 +1,62 @@
+﻿using Data.Model;
+using DataEF;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Repository
+{
+    public class PropertyGeneric<T> : IRepository<T> where T : Property
+    {
+        private AirbnbModel context;
+        private DbSet<T> PropertyEntity;
+        public PropertyGeneric(AirbnbModel context)
+        {
+            this.context = context;
+            PropertyEntity = context.Set<T>();
+        }
+       
+        public void Delete(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            T Property = GetById(id);
+            PropertyEntity.Remove(Property);
+            context.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return PropertyEntity.AsEnumerable();
+        }
+
+        public T GetById(int id)
+        {
+            return PropertyEntity.SingleOrDefault(s => s.PropertyId == id);
+        }
+
+        public void Insert(T obj)
+        {
+            context.Entry(obj).State = EntityState.Added;
+            context.SaveChanges();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T obj)
+        {
+            PropertyEntity.Attach(obj);
+            context.Entry(obj).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+    }
+}
